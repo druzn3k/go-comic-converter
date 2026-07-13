68050628030d535bb18a5eb49d345ec89508de89
Maurizio Del Corno <druznek@gmail.com>
2026-07-13 12:20:21 +0200

    feat: make jsonprogress.current atomic-safe (#19)

--- internal/pkg/epubprogress/json.go
+++ internal/pkg/epubprogress/json.go
@@ -2,21 +2,22 @@ package epubprogress
 
 import (
 	"encoding/json"
+	"sync/atomic"
 )
 
 type jsonprogress struct {
 	o       Options
 	e       *json.Encoder
-	current int
+	current atomic.Int64
 }
 
 func (p *jsonprogress) Add(num int) error {
-	p.current += num
+	p.current.Add(int64(num))
 	return p.e.Encode(map[string]any{
 		"type": "epubprogress",
 		"data": map[string]any{
 			"epubprogress": map[string]any{
-				"current": p.current,
+				"current": p.current.Load(),
 				"total":   p.o.Max,
 			},
 			"steps": map[string]any{
